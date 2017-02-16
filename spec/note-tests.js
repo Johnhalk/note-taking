bb7aee2ff5bc5bed13ac3f4234e095d4e600fcb1
John Ashton <John_ashton@hotmail.com>
2017-02-16 12:25:06 +0000

    added unique ID instantiation to every creation of note

--- spec/note-tests.js
+++ spec/note-tests.js
@@ -7,3 +7,12 @@ function testNoteInstantiation() {
 console.log("1. Has a method that will return the note text.")
 
 testNoteInstantiation();
+
+
+function testNotehasID() {
+  var note = new Note("Do I have an ID?");
+
+  assert.isTrue(note._ID === 1);
+}
+console.log("2. Note has unique assigned ID upon creation.")
+testNotehasID();
--- src/note-model.js
+++ src/note-model.js
@@ -1,6 +1,9 @@
 (function(exports) {
+    var numberid = 0
   function Note(text){
     this._text = text
+    this._ID = numberid
+    numberid++
   }
   Note.prototype.getText = function(){
     return this._text
