a45899215e35d602840db93f2135631c053018d7
John Ashton <John_ashton@hotmail.com>
2017-02-17 15:54:17 +0000

    challenge 12 complete. absolutely destroyed it.

--- src/note-controller.js
+++ src/note-controller.js
@@ -7,6 +7,15 @@
         element.innerHTML = this._viewList.displayList();
     };
 
+    Controller.prototype.newNoteSubmit = function(){
+    var noteInput = document.getElementById("text");
+
+    document.querySelector('form.note-form').addEventListener('submit',function(e){
+      e.preventDefault();
+      console.log(noteInput.value);
+    });
+    };
+
     Controller.prototype.urlChangeShowNote = function(){
       window.addEventListener("hashchange", showNoteForCurrentHash);
     };
